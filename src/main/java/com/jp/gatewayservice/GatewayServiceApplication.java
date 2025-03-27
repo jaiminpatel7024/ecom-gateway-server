42bd790e049b36469bae267a437d59f7cd265b6d
jaiminpatel7024 <jaiminpatel7024@yahoo.com>
2025-03-27 18:26:54 +0530

    Gateway Server changes

--- src/main/java/com/jp/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/jp/gatewayservice/GatewayServiceApplication.java
@@ -1,19 +1,17 @@
-package com.jp.configservice;
+package com.jp.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 
-@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigServiceApplication {
+public class GatewayServiceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(ConfigServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
 
     }
 
